51d5379fe18fccf83244f3846edaed0925b66595
Jai Sood <ankur.jai.sood@gmail.com>
2025-05-28 19:37:08 -0700

    Initial segformer test code

---
+++ run_segformer.py
@@ -0,0 +1,50 @@
+import torch
+import matplotlib.pyplot as plt
+from transformers import SegformerForSemanticSegmentation, SegformerFeatureExtractor
+from PIL import Image
+
+def run_segmformer(images):
+    model_name = "nvidia/segformer-b4-finetuned-ade-512-512"
+    feature_extractor = SegformerFeatureExtractor.from_pretrained(model_name)
+    model = SegformerForSemanticSegmentation.from_pretrained(model_name)
+
+    inputs = feature_extractor(images=images, return_tensors="pt")
+
+    with torch.no_grad():
+        outputs = model(**inputs)
+
+    logits = outputs.logits
+    predictions = torch.argmax(logits, dim=1)
+    return predictions
+
+def visualize_segmentation_masks(images, masks):
+    for img, mask in zip(images, masks):
+        mask_np = mask.cpu().numpy()
+
+        plt.figure(figsize=(12,5))
+        # original
+        plt.subplot(1,2,1)
+        plt.title("Input")
+        plt.imshow(img)
+        plt.axis("off")
+
+        # segmentation
+        plt.subplot(1,2,2)
+        plt.title("Segmentation")
+        plt.imshow(mask_np, cmap="nipy_spectral", interpolation="nearest")
+        plt.axis("off")
+
+        plt.show()
+
+
+if __name__ == "__main__":
+    image_paths = ["./datasets/kaggle-image-detection/test/images/000246247_jpg.rf.fb915aef7c063ce2ac971f8de0d8b2c1.jpg",
+                   "./datasets/kaggle-image-detection/test/images/004598275_jpg.rf.65c5b0c5f5b311f21b97690253580759.jpg"]
+    images = [Image.open(path).convert("RGB") for path in image_paths]
+
+    # Run Segmformer
+    masks = run_segmformer(images)
+    
+    # Visualize the segmentation masks
+    visualize_segmentation_masks(images, masks)
+
